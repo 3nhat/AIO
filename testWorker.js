dc7587c9f6252770ff481abf00b5247c7f884340
3nhat <102915327+3nhat@users.noreply.github.com>
2022-09-29 21:35:30 +0700

    Update testWorker.js

--- testWorker.js
+++ testWorker.js
@@ -1,15 +1,16 @@
 //function Q001(exp) {
 //      return exp.data;
 //}
+function T(e){
+     //var x = Q001(e);
+     postMessage(e.data);
+}
 
 function Q(a) {
    
   self.addEventListener('message', function(e) { T(e); }, false);  
 
-  function T(e){
-     //var x = Q001(e);
-     postMessage(e.data);
-  }
+  
 } 
 Q(1);
 
