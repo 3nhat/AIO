a83a0fa14a8646135ae14464150f278e7aabcd06
3nhat <102915327+3nhat@users.noreply.github.com>
2022-09-29 22:27:49 +0700

    Update testWorker.js

--- testWorker.js
+++ testWorker.js
@@ -1,25 +1,51 @@
 
-function loadFakeDOMforJQuery()
-{
-var document = self.document = {parentNode: null, nodeType: 9, toString: function() {return "FakeDocument"}};
-var window = self.window = self;
-var fakeElement = Object.create(document);
-fakeElement.nodeType = 1;
-fakeElement.toString=function() {return "FakeElement"};
-fakeElement.parentNode = fakeElement.firstChild = fakeElement.lastChild = fakeElement;
-fakeElement.ownerDocument = document;
+const webWorkerJQueryFix = () => {
+    document = self.document = { parentNode: null, nodeType: 9, toString: () => { return "FakeDocument" } };
+    window = self.window = self;
+    const fakeElement = Object.create(document);
+    fakeElement.nodeType = 1;
+    fakeElement.toString = () => { return "FakeElement" };
+    fakeElement.parentNode = fakeElement.firstChild = fakeElement.lastChild = fakeElement;
+    fakeElement.ownerDocument = document;
 
-document.head = document.body = fakeElement;
-document.ownerDocument = document.documentElement = document;
-document.getElementById = document.createElement = function() {return fakeElement;};
-document.createDocumentFragment = function() {return this;};
-document.getElementsByTagName = document.getElementsByClassName = function() {return [fakeElement];};
-document.getAttribute = document.setAttribute = document.removeChild = 
-document.addEventListener = document.removeEventListener = 
-function() {return null;};
-document.cloneNode = document.appendChild = function() {return this;};
-document.appendChild = function(child) {return child;};
-console.log("loaded FAKE DOM for JQuery");
+    document.innerHTML = "";
+    document.head =
+        document.body =
+        fakeElement;
+    document.ownerDocument =
+        document.documentElement =
+        document;
+
+    document.createElement =
+        document.getElementById =
+        document.getElementsByClassName =
+        document.getElementsByTagName = () => { return [fakeElement]; };
+
+    document.appendChild = (child) => { return child; };
+
+    document.createDocumentFragment = () => {
+        this.appendChild = document.appendChild;
+        return this;
+    };
+
+    document.getAttribute =
+        document.removeChild =
+        document.addEventListener =
+        document.removeEventListener =
+        document.setAttribute = () => { return null; };
+
+    document.cloneNode = () => {
+        this.cloneNode = document.cloneNode;
+        this.lastChild = {
+            checked: false,
+        }
+        return this;
+    };
+
+    document.childNodes = [];
+    document.implementation = {
+        createHTMLDocument: () => { return document; }
+    }
 }
 
 function T(exp){
@@ -28,7 +54,7 @@ function T(exp){
 }
 function Q(a) { self.addEventListener('message', function(e) { var x=T(e.data); postMessage(x); }, false); } 
 
-loadFakeDOMforJQuery();
+//loadFakeDOMforJQuery();
 //importScripts("/jquery-1.10.2.min.js");
 //importScripts("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js");
 
