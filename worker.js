b72c1cb4f766e8045c52baa7e17efa74283e90b4
3nhat <102915327+3nhat@users.noreply.github.com>
2022-09-29 10:35:10 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ var i=0;
 function timedCount()
 {
 i=i+1;
-postMessage(i);                   //posts a message back to the HTML page.
+postMessage(ten + '_' + i);                   //posts a message back to the HTML page.
 setTimeout("timedCount()",500);
 }
 
