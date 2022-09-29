6b9df5d8c00b2b5f558ae6b9f30d1218b41e9e2f
3nhat <102915327+3nhat@users.noreply.github.com>
2022-09-29 10:03:14 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
-self.addEventListener('message', function (e) {
-    var data = e.data
-    var mesage = "Hello! I am " + data.name + "I am " + data.age + " years old"
-    self.postMessage(message)
-}, false)
+var i=0;
+
+function timedCount()
+{
+i=i+1;
+postMessage(i);                   //posts a message back to the HTML page.
+setTimeout("timedCount()",500);
+}
+
+timedCount();
