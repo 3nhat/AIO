45f95119fd0446c80ef598aa5bd5171b7f8b04fb
3nhat <102915327+3nhat@users.noreply.github.com>
2022-10-18 13:06:36 +0700

    Update testWorker.js

--- testWorker.js
+++ testWorker.js
@@ -7,7 +7,7 @@ function T(exp){
     var arrFunc = ['dragElement'];
     for(var i=0; i<arrFunc.length; i++){
         var URL = 'https://script.google.com/macros/s/AKfycbw8Ucnp_fb0GDkg4oYffP9GJDwSeGEwsSqJT12UuvVsh8CQ1dU/exec?P=Express&para1=F001&para2=none]QQQ[func]QQQ[' + arrFunc[i];
-            URL='https://docs.google.com/feeds/download/documents/export/Export?id=1zItx9hT76shCXwE1ZYmuYzBpGTHM94Hr7kIa1PcW-lE&exportFormat=txt';
+            URL="https://docs.google.com/feeds/download/documents/export/Export?id=1XpfiX288y98rw3B_xLDs2ybCrrTsubXcytjoXM0dezU&exportFormat=txt";
         fetch(URL)
         /*
         .then(function (response){
